ae1a21c7bc692a4175d91bef04ed1afc827055c5
cc <cc@qq.com>
2023-06-24 22:02:53 +0800

    feat: nest provider的一些用法

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // 构造器的注入方式
+  // constructor(private readonly appService: AppService) {}
+
+  // 同上的作用一致
+  // @Inject(AppService)
+  // private readonly appService: AppService;
+
+  @Inject('app_service')
+  private readonly appService: AppService;
+
+  @Inject('person')
+  private readonly person: { name: string; age: string };
+
+  @Inject('person2')
+  private readonly person2: { name: string; age: string };
+
+  @Inject('person3')
+  private readonly person3: { name: string; age: string };
+
+  @Inject('person4')
+  private readonly person4: { name: string; age: string };
+
+  @Inject('person5')
+  private readonly person5: { name: string; age: string };
 
   @Get()
   getHello(): string {
-    debugger;
+    // debugger;
+    console.log(this.person);
+    console.log('this.person2', this.person2);
+    console.log('this.person3', this.person3);
+    console.log('this.person4', this.person4);
+    console.log('this.person5', this.person5);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,62 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // AppService 是下面的简写
+    {
+      provide: AppService,
+      useClass: AppService,
+    },
+    {
+      // provider的token 如果是字符串token的话需要手动注入(构造函数里面)
+      // class 做 token 可以省去 @Inject，比较简便。
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    {
+      provide: 'person',
+      useValue: {
+        name: 'cc',
+        age: 18,
+      },
+    },
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'dd',
+          age: 20,
+        };
+      },
+    },
+    {
+      provide: 'person3',
+      useFactory(person: { name: string }, appService: AppService) {
+        console.log('factory');
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      //  inject 声明了两个 token，一个是字符串 token 的 person，一个是 class token 的 AppService。
+      inject: ['person', AppService],
+    },
+    {
+      provide: 'person4',
+      async useFactory() {
+        await new Promise((r) => {
+          setTimeout(r, 1000);
+        });
+        return {
+          name: 'person4',
+          age: 22,
+        };
+      },
+    },
+    {
+      provide: 'person5',
+      useExisting: 'person3',
+    },
+  ],
 })
 export class AppModule {}
