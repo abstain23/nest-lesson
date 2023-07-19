5dfd5944e09ee67e420ed8fe968ca5c765b38409
jieyang <jieyang@deeproute.ai>
2023-07-19 14:51:43 +0800

    feat: update gif 压缩

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,62 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { existsSync } from 'fs';
+import * as sharp from 'sharp';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+    return file.path;
+  }
+
+  @Get('compression')
+  async compression(
+    @Query('path') filePath: string,
+    @Query('color', ParseIntPipe) color: number,
+    @Query('level', ParseIntPipe) level: number,
+    @Res() res: Response,
+  ) {
+    console.log(filePath, color, level);
+
+    if (!existsSync(filePath)) {
+      throw new BadRequestException('文件不存在');
+    }
+
+    const data = await sharp(filePath, {
+      animated: true,
+      limitInputPixels: false,
+    })
+      .gif({
+        // compressionLevel: level,
+        colours: color,
+      })
+      .toBuffer();
+
+      res.set('Content-Disposition', `attachment; filename="dest.gif"`);
+
+      res.send(data);
+  }
 
   @Get()
   getHello(): string {
