f838f32cb24e74116966a4ab0bf92131267c78a1
jieyang <jieyang@deeproute.ai>
2023-07-24 14:09:37 +0800

    feat: swagger 文档测试

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Query, UnauthorizedException } from '@nestjs/common';
+import { ApiBasicAuth, ApiBearerAuth, ApiBody, ApiCookieAuth, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { CccDto } from './ccc.dto';
+import { CccVo } from './ccc.vo';
 
-@Controller()
+@ApiTags('xxx')
+@Controller('xxx')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -9,5 +13,79 @@ export class AppController {
   getHello(): string {
     debugger;
     return this.appService.getHello();
+  }
+
+
+  @ApiBearerAuth('bearer')
+  @ApiTags('xxx-get')
+  @ApiOperation({summary: '测试 aaa', description: 'aaa 描述'})
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'aaa 成功',
+    type: String
+  })
+  @ApiQuery({
+    name: 'a1',
+    type: String,
+    description: 'a1 param',
+    required: false,
+    example: '1111'
+  })
+  @ApiQuery({
+    name: 'a2',
+    type: String,
+    description: 'a2 param',
+    required: true,
+    example: '222'
+  })
+  @Get('aaa')
+  aaa(@Query('a1') a1, @Query('a2') a2) {
+    console.log(a1, a2);
+    return 'aaa success';
+  }
+
+  @ApiCookieAuth('cookie')
+  @ApiOperation({summary: '测试 bbb', description: 'bbb 描述'})
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'bbb 成功',
+    type: String
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: '未认证的ID'
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'ID',
+    required: true,
+    example: 2222
+  })
+  @Get('bbb/:id')
+  bbb(@Param('id') id) {
+    if(id !== 1111) {
+      throw new UnauthorizedException()
+    }
+    console.log(id, typeof id);
+    return 'bbb success';
+  }
+
+  @ApiBasicAuth('basic')
+  @ApiOperation({summary: '测试 ccc', description: 'ccc 描述'})
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'ccc 成功',
+    type: CccVo
+  })
+  @ApiBody({
+    type: CccDto
+  })
+  @Post('ccc')
+  ccc(@Body('ccc') ccc: CccDto) {
+    console.log(ccc);
+    const vo = new CccVo();
+    vo.aaa = 111;
+    vo.bbb = 222;
+    return vo;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/static' });
+
+  const config = new DocumentBuilder()
+    .setTitle('Test Example')
+    .setDescription('The Api Desc')
+    .setVersion('1.0')
+    .addTag('test_tag')
+    .addBasicAuth({
+      type: 'http',
+      name: 'basic',
+      description: '用户名 + 密码'
+    })
+    .addCookieAuth('session_id', {
+      type: 'apiKey',
+      name: 'cookie',
+      description: '基于cookie的认证'
+    })
+    .addBearerAuth({
+      type: 'http',
+      description: '基于jwt的认证',
+      name: 'bearer'
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
