d69cd7814165cef6b6f1184fecbb6f9ad5a698b8
jieyang <jieyang@deeproute.ai>
2023-08-30 17:11:56 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as session from 'express-session'
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(session({
+    secret: 'cc',
+    resave: false,
+    saveUninitialized: false
+  }))
+
   app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/static' });
   await app.listen(3000);
 }
