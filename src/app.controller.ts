8915de9c9616785126490102d378776f1e03ab5e
jieyang <jieyang@deeproute.ai>
2023-09-12 15:41:57 +0800

    feat: update qrcode login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { randomUUID } from 'crypto';
+import * as qrcode from 'qrcode';
+
+const map = new Map<string, QrCodeInfo>();
+
+interface QrCodeInfo {
+  status:
+    | 'noscan'
+    | 'scan-wait-confirm'
+    | 'scan-confirm'
+    | 'scan-cancel'
+    | 'expired';
+  userInfo?: {
+    userId: number;
+  };
+}
+// noscan 未扫描
+// scan-wait-confirm -已扫描，等待用户确认
+// scan-confirm 已扫描，用户同意授权
+// scan-cancel 已扫描，用户取消授权
+// expired 已过期
 
 @Controller()
 export class AppController {
@@ -9,5 +30,55 @@ export class AppController {
   getHello(): string {
     debugger;
     return this.appService.getHello();
+  }
+
+  @Get('qrcode/generate')
+  async generate() {
+    const uuid = randomUUID();
+    const dataUrl = await qrcode.toDataURL(
+      `http://10.24.33.120:3000/static/confirm.html?uuid=${uuid}`,
+    );
+    map.set(`qrcode_${uuid}`, {
+      status: 'noscan',
+    });
+    return {
+      qrcode: uuid,
+      img: dataUrl,
+    };
+  }
+
+  @Get('qrcode/check')
+  async check(@Query('id') id: string) {
+    return map.get(`qrcode_${id}`);
+  }
+
+  @Get('qrcode/scan')
+  async scan(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-wait-confirm';
+    return 'success';
+  }
+
+  @Get('qrcode/confirm')
+  async confirm(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-confirm';
+    return 'success';
+  }
+
+  @Get('qrcode/cancel')
+  async cancel(@Query('id') id: string) {
+    const info = map.get(`qrcode_${id}`);
+    if (!info) {
+      throw new BadRequestException('二维码已过期');
+    }
+    info.status = 'scan-cancel';
+    return 'success';
   }
 }
